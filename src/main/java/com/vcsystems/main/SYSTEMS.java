fbb7502e704d5c7b8876ca38451bdc906d4ef7ab
MatiasCarmen <U22209935@utp.edu.pe>
2025-06-25 02:07:55 -0500

    primer commit estructura del proyecto

---
+++ src/main/java/com/vcsystems/main/SYSTEMS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.vcsystems.main;
+
+/**
+ *
+ * @author mathi
+ */
+public class SYSTEMS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/com/vcsystems/modelo/entidades/Proveedor.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.vcsystems.modelo.entidades;
+
+/**
+ *
+ * @author mathi
+ */
+public class Proveedor {
+    
+}
